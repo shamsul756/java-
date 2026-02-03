14ebe66034eea8e3d914de3c28dfb71154a4fbd1
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-04 00:20:00 +0600

    using max and min complte a simple work

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function noDouplicate (array){
-    const unique = [];
-    for (const item of array){
-    if (unique.includes(item) === false){
-        unique.push(item);
+// function noDouplicate (array){
+//     const unique = [];
+//     for (const item of array){
+//     if (unique.includes(item) === false){
+//         unique.push(item);
     
-    }
-    }
-    return unique;
-}
-const birianiLover = [ 'abul ', 'balu', 'nalo', 'abul', 'balu', 'rahsed', 'habib'];
-const numbers = [33, 44, 4, 55, 44, 33, 44, 42, 12];
-const uniqueArray = noDouplicate(birianiLover);
-console.log(uniqueArray);
+//     }
+//     }
+//     return unique;
+// }
+// const birianiLover = [ 'abul ', 'balu', 'nalo', 'abul', 'balu', 'rahsed', 'habib'];
+// const numbers = [33, 44, 4, 55, 44, 33, 44, 42, 12];
+// const uniqueArray = noDouplicate(birianiLover);
+// console.log(uniqueArray);
 
 
+// const min = Math.min(22, 44, 5, 4, 44, 332, 1)
+//     console.log(min);
+//min means which number is smaller then other when we are trying to get smaller number we use min otherwise we use max ( which are given bigger number)
 
+const min = Math.min(333, 44, 54, 33, 42, 2);
+console.log(min);
 
+const max = Math.max(33, 22, 3333333, 4, 444, 32, 3);
+console.log(max);
