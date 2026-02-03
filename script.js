ca75bf57e941162fc6a104e5bb12b2c872750514
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-03 23:49:33 +0600

    remove duplicate item form aray

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const birianiLover = [ 'abul ', 'balu', 'nalo', 'abul', 'balu', 'rahsed', 'habib'];
-const numbers = [33, 44, 4, 55, 44, 33, 44, 42, 12]{
+function noDouplicate (array){
+    const unique = [];
+    for (const item of array){
+    if (unique.includes(item) === false){
+        unique.push(item);
     
+    }
+    }
+    return unique;
 }
+const birianiLover = [ 'abul ', 'balu', 'nalo', 'abul', 'balu', 'rahsed', 'habib'];
+const numbers = [33, 44, 4, 55, 44, 33, 44, 42, 12];
+const uniqueArray = noDouplicate(birianiLover);
+console.log(uniqueArray);
+
+
+
+
