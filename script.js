e038ac757a8244573cb4c852c7748ba00eb9668c
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-04 23:54:24 +0600

    check ever or odd number

--- script.js
+++ script.js
@@ -1,221 +1,62 @@
-// function noDouplicate (array){
-//     const unique = [];
-//     for (const item of array){
-//     if (unique.includes(item) === false){
-//         unique.push(item);
-    
-//     }
-//     }
-//     return unique;
+// function add (num1, num2){
+//   return num1 + num2;
 // }
-// const birianiLover = [ 'abul ', 'balu', 'nalo', 'abul', 'balu', 'rahsed', 'habib'];
-// const numbers = [33, 44, 4, 55, 44, 33, 44, 42, 12];
-// const uniqueArray = noDouplicate(birianiLover);
-// console.log(uniqueArray);
+// function subtract (num1, num2){
 
-
-// const min = Math.min(22, 44, 5, 4, 44, 332, 1)
-//     console.log(min);
-//min means which number is smaller then other when we are trying to get smaller number we use min otherwise we use max ( which are given bigger number)
-
-// const min = Math.min(333, 44, 54, 33, 42, 2);
-// console.log(min);
-
-// const max = Math.max(33, 22, 3333333, 4, 444, 32, 3);
-// console.log(max);
-
-// console.log(Math.abs(22-10));
-// console.log(Math.round(33.943));
-// console.log(Math.floor(44.98888832));
-// console.log(Math.ceil(3.011));
-// console.log(Math.random(1.525));
-
-// const today = new Date();
-// const date = new Date (2026 / 10 / 19);
-// console.log(date.getMonth());
-// console.log(date.getDay());
-// const specificDate = new Date(2091, 0, 26);
-// console.log(specificDate);
-// specificDate .setMonth(10);
-// console.log(specificDate);
-// console.log(specificDate.toLocaleDateString('en-GB'));
-
-
-// let a = 5;
-// let b = 7;
-// console.log(a, b);
-// const temp = a;
-// a = b;
-// b = temp;
-
-
-// console.log(a, b);
-// // temp means a function when we are trying to change one value to another then we use temp.
-// let a = 4;
-// let b = 5;
-// console.log(a, b);
-// [a, b] = [b, a];
-// console.log(a, b);
-
-// const afroja = 23;
-// const sadiya = 25;
-// if (afroja > sadiya){
-//     console.log('sadiya will be a get a apple');
+//   return num1 - num2;
 // }
-// else {
-//     console.log('afroja will be eat an apple');
+// function multiple (num1, num2){
+//   return num1 * num2;
 // }
-
-// function getMax (num1, num2){
-//     if (num1 > num2){
-//         return num1;
-//     }
-//     else {
-//         return num2;
-//     }
+// function divide (num1, num2){
+//   return num1 / num2;
 // }
-// const max = getMax(42, 10);
-// console.log('max of two is:', max);
-
-// const rahim = 24;
-// const banki = 34;
-// const bulki = 54;
-// if (rahim > bulki && rahim > banki ){
-//     console.log('rahim is the ultimately boss');
-// }
-// else if (banki > rahim && banki > bulki){
-//     console.log('banki is the superboss');
-// }
-// else {
-//     console.log('bulki is the most powerful boss');
-// }
-// function maxOfThree (num1, num2, num3){
-//     if(num1 >num2 && num1 >num3){
-//         return num1;
-//     }
-//     else if(num2 >num1 && num2 >num3){
-//         return num2;
-//     }
-//     else {
-//         return num3;
-//     }
-// }
-// const max = maxOfThree(354, 443, 333);
-// const result = max;
-// console.log(result);
-
-// const heights = [ 23, 42, 41, 40, 56, 57];
-
-// function getMax(number)
-
-// {
-//      let maximum = number[0];
-//    for (const num of number){
-   
-//     if(num > maximum){
-//         maximum = num;
-//     }
-//    }
-//    return maximum;
-// }
-
-// const output = getMax(heights);
-// // // 
-// // console.log('max value is ', output);
-// const prices = [998, 55, 3333, 55555, 3354532, 4444654 ];
-// function getMin(numbers){
-//   let min = numbers[0]
-// for (const number of numbers ){
-//   if(number < min){
-//     min = number;
+// function calculator(a, b, opearation){
+//   if(opearation === 'add'){
+//     const result = add (a, b);
+//     return result;
 //   }
-// }
-// return min;
-
-// }
-// const cheap = getMin(prices);
-// console.log('cheapest one is :', cheap);
-
-// const prices = [33, 44, 55, 22, 55, 33];
-// function gitMin(numbers){
-//   let min = numbers [0];
-//   for(const number of numbers){
-//     if(number < min){
-//       min = number;
-//     }
+//   else if(opearation === 'subtract'){
+//     const result = subtract (a, b);
 //   }
-//   return min;
-// }
-// const chep = gitMin(prices);
-// console.log('cheapest one is:', chep);
-
-// const prices = [332, 44, 5533, 5533, 55];
-// function any(results){
-//   let min = results [0];
-//   for(const result of results){
-//     if(result < min){
-//       min = result;
-//     }
+//   else if (opearation === 'multiply'){
+//    return multiply(a, b);
 //   }
-//   return min;
-// }
-// const cheapest = any (prices);
-// console.log('most cheapest number ever:', cheapest);
-
-// const mobiles = [
-
-//   {name: 'realme', color: 'off-white', prize: 1241, camera : '10mp'},
-//   {name: 'redme', color: 'off-white', prize: 254456, camera : '12mp'},
-//   {name: 'apple', color: 'off-white', prize: 23222 , camera : '12mp'},
-//   {name: 'oppo', color: 'off-white', prize:12574, camera : '14mp'}
-
-// ]
-// function getCheapestMobiles(mobiles){
-//   let max = mobiles[0]
-//  for(const mobile of mobiles){
-//  if(mobile.prize > max.prize){
-//   max = mobile ;
-//  }
-//  }
-//  return max;
-// }
-
-// const result =  getCheapestMobiles(mobiles);
-// console.log('most chepest mobiles', result);
-
-// const products = [
-// {name: 'shamspu', price : 300, quantity :5 },
-// {name: 'watch', price : 1500, quantity :4 },
-// {name: 'pant', price : 1300, quantity :3 },
-// {name: 'tea-shirt', price : 800, quantity :2 }
-
-// ]
-// function carTotal (products){
-//   let total = 0;
-//   for (const product of products){
-//     let thisProductCost = product.price * product.quantity;
-//     total = total + thisProductCost;
+//   else if(opearation === 'divide'){
+//     return divide (a, b)
 //   }
-//   return total;
+//   else {
+//     return "only  'add', 'subtract', 'multiple', 'divide' opearation is allowed."
+//   }
+//   }
+//   const result = calculator(5, 7, 'add');
+//   console.log(result);
+
+
+// function blender(fruit){
+//   console.log('i making a fruit juice', fruit);
 // }
-// const totalShopping = carTotal(products);
-// console.log(totalShopping);
+// blender('apple')
+// let num1 = 24;
+// let num2 = 20;
 
-const products =[
-{name :'t-shirt',  price : 340, quantity: 2},
-{name : 'pant', price : 540, quantity :3},
-{name : 'under-pant', price : 300, quantity: 2},
-{name : 'shampu', price : 400, quantity: 3}
+// let sum = num1 + 2;
+// if (sum % 2 === 0){
+//   console.log('this is a even number')}
+//   else {
+//     console.log('it is not a even numbers , so we can say it is odd number')
+//   }
 
-]
-function shoppingCart (products){
-  let total = 0;
-  for(const product of products){
-    let totalAllCost = product.price * product.quantity;
-    total = total + totalAllCost;
+function checkEvenOrOdd(num1, num2){
+ 
+  let sum = num1 + num2;
+   console.log(sum);
+  if(sum % 2 === 0){
+    console.log('it is an even number');
   }
-  return total;
+  else{
+    console.log('it is an odd number');
+  }
 }
-const totalCost = shoppingCart(products);
-
-console.log(totalCost);
+ checkEvenOrOdd (33, 44);
+ checkEvenOrOdd (30, 102)
